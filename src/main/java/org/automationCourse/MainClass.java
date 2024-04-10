62a23cfb4c01d5f4e7def42e213e6125d608aaa4
arodkina <arodkina@devexperts.com>
2024-04-10 18:36:13 +0400

    Homework 1.2

--- src/main/java/org/automationCourse/MainClass.java
+++ src/main/java/org/automationCourse/MainClass.java
@@ -3,8 +3,13 @@ package org.automationCourse;
 public class MainClass {
 
     private static final int LOCAL_NUMBER = 14;
+    private static final int CLASS_NUMBER = 20;
 
     public static int getLocalNumber() {
         return LOCAL_NUMBER;
+    }
+
+    public static int getClassNumber() {
+        return CLASS_NUMBER;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -2,6 +2,7 @@ import org.automationCourse.MainClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MainClassTest {
 
@@ -10,5 +11,10 @@ public class MainClassTest {
     @Test
     public void testGetLocalNumber() {
         assertEquals("Method getLocalNumber() returned unexpected value", EXPECTED_LOCAL_NUMBER, MainClass.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        assertTrue("Method getClassNumber() returned unexpected value, expected > 45,  but was < 45", MainClass.getClassNumber() > 45);
     }
 }
