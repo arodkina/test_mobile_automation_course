80f2d58bad73631c7a8b93dd55cbbf44afafc040
arodkina <arodkina@devexperts.com>
2024-04-10 18:02:36 +0400

    remove comments

--- src/main/java/org/automationCourse/MainClass.java
+++ src/main/java/org/automationCourse/MainClass.java
@@ -1,7 +1,5 @@
 package org.automationCourse;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class MainClass {
 
     private static final int LOCAL_NUMBER = 14;
